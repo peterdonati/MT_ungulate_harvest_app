99286444fefc83e6d8bac5a98a4282c75e52acea
peterdonati <65191186+peterdonati@users.noreply.github.com>
2020-10-07 15:28:34 -0600

    adding multiple HDs
    - added functionality for multiple H.D.s at once
    -fixed error when no district selected

--- R/app.R
+++ R/app.R
@@ -16,26 +16,30 @@ elk$District <- as.character(elk$District)
 
 # UI ===========================================================================
 ui <- fluidPage(
+  
   theme = shinytheme("flatly"),
   
   titlePanel("Montana ungulate harvest estimates by district"),
+  
   sidebarLayout(
     sidebarPanel(
-      textInput(
+      selectInput(
         inputId = "dist",
-        label = "Hunting district you want to see:
-      (Invalid districts will create empty plots)",
+        label = "Hunting district(s) you want to see:",
+        choices = unique(elk$District),
+        multiple = TRUE
       ),
       selectInput(
         inputId = "split",
-        label = "Split data by:",
+        label = "Split by:",
         choices = c("Nothing" = "Nothing", 
                     "Sex" = "Sex", 
                     "Weapon type" = "Weapon"),
       )
     ),
+    
     mainPanel(
-      plotOutput(outputId = "plot", height = 600, width = 800)
+      plotOutput(outputId = "plot", height = 700, width = 1000),
     )
   )
 )
@@ -49,46 +53,52 @@ server <- function(input, output, session){
     panel.border = element_rect(fill = NA, color = "black"),
     legend.key = element_rect(fill = NA)
   )
+  
+  # Later used in scale_x_continuous() to standardize x axis for when
+  # districts are missing data for some years:
   years <- 2015:2019
   
+  # Plot creation:
   output$plot <- renderPlot({
     
-    # Filter to desired district:
-    plot_dat <- elk %>% 
-      filter(District == input$dist)
-    
-    if (input$split == "Nothing"){
-      return(
-        ggplot(plot_dat, aes(x = Year, y = N)) +
-          geom_line() +
-          geom_point(size = 2) +
-          labs(
-            title = paste("Elk harvest estimate for HD", input$dist),
-            y = "Estimated harvest"
-          ) +
-          scale_x_continuous(breaks = years, minor_breaks = F) +
-          mytheme
-      )
-    } else {
-      # Defining y variable and split:
-      if (input$split == "Sex") { 
-        y_var <- sym("n_sex")
-      } else if (input$split == "Weapon") {
-        y_var <- sym("n_weapon")
-      } 
-      split <- sym(input$split)
+    if (!is.null(input$dist)){
+      # Filter to desired district(s):
+      plot_dat <- elk[which(elk$District %in% input$dist), ]
       
-      return(
-        ggplot(plot_dat, aes(x = Year, y = !!y_var)) +
-          geom_line(aes(color = !!split)) +
-          geom_point(aes(color = !!split), size = 2) + 
-          labs(
-            title = paste("Elk harvest estimate for HD", input$dist),
-            y = "Estimated harvest"
-          ) +
-          scale_x_continuous(breaks = years, minor_breaks = F) +
-          mytheme
-      )
+      if (input$split == "Nothing"){
+        return(
+          ggplot(plot_dat, aes(x = Year, y = N)) +
+            geom_line(aes(color = District)) +
+            geom_point(size = 2) +
+            labs(
+              title = "Elk harvest estimates",
+              y = "Estimated harvest"
+            ) +
+            scale_x_continuous(breaks = years, minor_breaks = F) +
+            mytheme
+        )
+      } else {
+        # Defining y variable and split:
+        if (input$split == "Sex") { 
+          y_var <- sym("n_sex")
+        } else if (input$split == "Weapon") {
+          y_var <- sym("n_weapon")
+        } 
+        split <- sym(input$split)
+        
+        return(
+          ggplot(plot_dat, aes(x = Year, y = !!y_var)) +
+            geom_line(aes(linetype = !!split)) +
+            geom_point(size = 2) + 
+            facet_wrap(~District, ncol = 2) +
+            labs(
+              title = "Elk harvest estimates",
+              y = "Estimated harvest"
+            ) +
+            scale_x_continuous(breaks = years, minor_breaks = F) +
+            mytheme
+        )
+      }
     }
   })
 }
