7e66a00646fcb5d058ebca1103c339ec5c8bd108
peterdonati <65191186+peterdonati@users.noreply.github.com>
2020-10-12 00:22:35 -0600

    deer & elk
    - Added data and functionality for deer and antelope.
    - Added year slider.

--- R/app.R
+++ R/app.R
@@ -10,11 +10,9 @@ library(ggplot2)
 library(rlang)
 library(shinythemes)
 
-elk <- read.csv(text = getURL("https://raw.githubusercontent.com/peterdonati/MT_ungulate_harvest_app/main/Datasets/elk_harvest.csv"))
-elk$District <- as.character(elk$District)
-
-deer <- read.csv(text = getURL("https://raw.githubusercontent.com/peterdonati/MT_ungulate_harvest_app/main/Datasets/deer_harvest.csv"))
-deer$District <- as.character(deer$District)
+#h_dat <- read.csv(text = getURL("https://raw.githubusercontent.com/peterdonati/MT_ungulate_harvest_app/main/Datasets/harvest_dat.csv"))
+h_dat <- read.csv("C:/Users/Peter/Documents/GitHub/MT_ungulate_harvest_app/Datasets/harvest_dat.csv")
+h_dat$District <- as.character(h_dat$District)
 ################################################################################
 
 # UI ===========================================================================
@@ -28,75 +26,78 @@ ui <- fluidPage(
     sidebarPanel(
       selectInput(
         inputId = "ung_sp",
-        label = "Explore dharvest estimates for:",
-        choices = c("Deer", "Elk")
+        label = "Species",
+        choices = c("Deer" = "deer", 
+                    "Elk" = "elk",
+                    "Antelope" = "ant")
       ),
-      sidebarPanel(
-        selectInput(
-          inputId = "sp",
-          label = "Explore dharvest estimates for:",
-          choices = c("Deer", "Elk")
-        ),
-        sliderInput(
-          inputID = "yr",
-          label = "Years:",
-          min = 2010,
-          max = 2019,
-          value = c(2010, 2019),
-          step = 1,
-          ticks = FALSE
-        ),
-      
-        # Need to make separate UI for each species because of differing data
-      conditionalPanel(
-        condition = "input.sp == Elk",
-        selectInput(
-          inputId = "dist",
-          label = "Hunting district(s) you want to see:",
-          choices = unique(elk$District),
-          multiple = TRUE
-        ),
-        selectInput(
-          inputId = "split",
-          label = "Split by:",
-          choices = c("Just the districts I chose" = "Nothing", 
-                      "Sex" = "Sex", 
-                      "Weapon type" = "Weapon"),
-        )
-      )
-    ),
-    
-    conditionalPanel(
-      condition = "input.sp == Deer",
+      sliderInput(
+        inputId = "yr",
+        label = "Years",
+        min = 2010,
+        max = 2019,
+        value = c(2010, 2019),
+        step = 1,
+        ticks = FALSE,
+        sep = ""
+      ),
       selectInput(
         inputId = "dist",
-        label = "Hunting district(s) you want to see:",
-        choices = unique(deer$District),
+        label = "Districts",
+        choices = NULL,
         multiple = TRUE
       ),
-      selectInput(
-        inputId = "deer_sp",
-        label = "Species:",
-        choices = c("Whitetail", "Mule", "Combined")
-      ),
-      selectInput(
-        inputId = "split",
-        label = "Split by:",
-        choices = c("Just the districts I chose" = "Nothing", 
-                    "Sex" = "Sex", 
-                    "Weapon type" = "Weapon"),
-      )
-    )
-  ),
+      uiOutput("split"),
+      uiOutput("deer_sp")
+    ),
     
     mainPanel(
       plotOutput(outputId = "plot", height = 700, width = 1000),
+      textOutput("nodata")
     )
   )
 )
 
 # server =======================================================================
 server <- function(input, output, session){
+  
+  # UI for split data (FWP doesn't publish weapon type for antelope)
+  output$split <- renderUI({
+    if(input$ung_sp != "ant")
+      selectInput(
+        inputId = "split",
+        label = "Split",
+        choices = c("Nothing", "Weapon", "Sex")
+      ) else if (input$ung_sp == "ant"){
+        selectInput(
+          inputId = "split",
+          label = "Split",
+          choices = c("Nothing", "Sex")
+        )
+      }
+  })
+    
+  # Need to show this UI if looking at deer harvest:
+  output$deer_sp <- renderUI({
+    if (input$ung_sp == "deer"){
+      selectInput(
+        inputId = "deer_sp",
+        label = "Deer species",
+        choices = c("Combined" = "all_deer",
+                    "Whitetail" = "wt", 
+                    "Mule" = "md")
+      )
+    }
+  })
+  
+  # Update input$dist choices according to species:
+  district_choice <- reactive({
+    filter(h_dat, ung == input$ung_sp)
+  })
+  observeEvent(district_choice(), {
+    updateSelectInput(session, "dist",
+                      choices = unique(district_choice()$District))
+  })
   
   mytheme <- theme(
     text = element_text(family = "serif", size = 18),
@@ -105,36 +106,39 @@ server <- function(input, output, session){
     legend.key = element_rect(fill = NA)
   )
   
-  # Later used in scale_x_continuous() to standardize x axis for when
-  # districts are missing data for some years:
-  years <- 2015:2019
-  
   # Plot creation:
   output$plot <- renderPlot({
     
-    if (!is.null(input$dist)){
-      #Generating a new dataset containing only selected variables:
-      
-      if (input$ung_sp == "Elk"){
-        plot_dat <- elk[which(elk$District %in% input$dist), ]
-      } else if (input$ung_sp == "Deer"){
-        plot_dat <- deer[which(deer$District %in% input$dist), ] %>% 
-          
+    # Later used in scale_x_continuous() to standardize x axis for when
+    # districts are missing data for some years:
+    years <- input$yr[[1]]:input$yr[[2]]
+    
+      #Generate a new dataset containing only selected variables:
+      plot_dat <- filter(h_dat, ung == input$ung_sp)
+      plot_dat <- plot_dat[which(plot_dat$Year %in% years), ]
+      plot_dat <- plot_dat[which(plot_dat$District %in% input$dist), ]
+      if (input$ung_sp == "deer"){
+        plot_dat <- filter(plot_dat, Species == input$deer_sp)
       }
+    
+    if (!is.null(input$dist)){
       
-      if (input$split == "Nothing"){
+      # GRAPH FOR EFFORT OR NO SPLIT:
+      if (input$split == "Nothing" || is.null(input$split)){
+        
         return(
-          ggplot(plot_dat, aes(x = Year, y = N)) +
-            geom_line(aes(color = District)) +
+          ggplot(plot_dat, aes(x = Year, y = N, color = District)) +
+            geom_line() +
             geom_point(size = 2) +
             labs(
-              title = "Elk harvest estimates",
               y = "Estimated harvest"
             ) +
             scale_x_continuous(breaks = years, minor_breaks = F) +
             mytheme
         )
       } else {
+        
+        # GRAPH FOR SPLITS:
         # Defining y variable and split:
         if (input$split == "Sex") { 
           y_var <- sym("n_sex")
@@ -149,7 +153,6 @@ server <- function(input, output, session){
             geom_point(size = 2) + 
             facet_wrap(~District, ncol = 2) +
             labs(
-              title = "Elk harvest estimates",
               y = "Estimated harvest"
             ) +
             scale_x_continuous(breaks = years, minor_breaks = F) +
@@ -160,9 +163,9 @@ server <- function(input, output, session){
       # Placeholder for when no district is chosen:
       ggplot() +
         annotate("text", 
-                 label = "Pick a district!", 
-                 x = .5, y= .5, 
-                 size = 16,
+                 label = "Plot created once district chosen", 
+                 x = 0, y= 0, 
+                 size = 7,
                  family = "serif") +
         theme_void() +
         theme(
@@ -170,6 +173,24 @@ server <- function(input, output, session){
         )
     }
   })
+
+  
+  # NEED TO FIGURE THIS OUT FOR CASES WHEN DATA IS MISSING IN YEARS
+  # output$nodata <- renderText({
+  #   plot_dat <- filter(h_dat, ung == input$ung_sp)
+  #   plot_dat <- plot_dat[which(plot_dat$Year %in% years), ]
+  #   plot_dat <- plot_dat[which(plot_dat$District %in% input$dist), ]
+  #   if (input$ung_sp == "deer"){
+  #     plot_dat <- filter(plot_dat, Species == input$deer_sp)
+  #   }
+  #   
+  #   if (!all(input$dist %in% plot_dat$District)){
+  #   paste("No data for district",
+  #         input$dist[[which(!input$dist %in% plot_dat$District)]],
+  #         "for selected years")
+  #   } else {""}
+  # })
+
 }
 
 
